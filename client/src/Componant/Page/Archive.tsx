4f6dbf356a7e7399efefd5b253d2b64640c728d3
xxpp3 <azedinezebiri@gmail.com>
2023-02-17 18:07:19 +0100

    putted globalStyle

--- client/src/Componant/Page/Archive.tsx
+++ client/src/Componant/Page/Archive.tsx
@@ -1,26 +1,30 @@
-import React from 'react'
-import {Items} from "../Style/Style"
+import React from "react";
+import { Items } from "../Style/Style";
 const array = [
-    {titre:"hmidan",
-    date:"12:53",
-    text:"this is garbage dont mind it at all"
-    },
-    {titre:"hmidan",
-    date:"12:53",
-    text:"this is garbage dont mind it at all"
-    },
-]
+  {
+    titre: "hmidan",
+    date: "12:53",
+    text: "this is garbage dont mind it at all",
+  },
+  {
+    titre: "hmidan",
+    date: "12:53",
+    text: "this is garbage dont mind it at all",
+  },
+];
 export default function Archive() {
   return (
-    <div>
-        {array.map((item,index)=>(
-            <Items key={index}>
-                <p><b>{item.titre}</b></p>
-                <p>{item.text}</p>
-                <p>{item.date}</p>
-            </Items>
-        ))}
-        
-    </div>
-  )
+  <div>
+    {array.map((item, index) => (
+    <Items key={index}>
+      <p>
+        <b>{item.titre}</b>
+      </p>
+      <p>{item.text}</p>
+      <p>{item.date}</p>
+    </Items>
+  ))}
+  </div>)
+  
+    
 }
